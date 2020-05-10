e61279e56547e4576d1048c8cc6df2e9196d590e
StarryPlenilune <1205832940@qq.com>
2020-05-10 21:19:33 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	dome/src/com/zfc/TestMin.java

--- dome/src/com/zfc/Test.java
+++ dome/src/com/zfc/Test.java
@@ -1,9 +1,7 @@
 package com.zfc;
 
-public class TestMain {
+public class Test {
     public static void main(String[] args) {
-        System.out.println("周凡程");
         System.out.println("李大炮");
-
     }
 }
