18894abb96d2d1a8fbf57ca22a3e0be0d876cc9f
StarryPlenilune <1205832940@qq.com>
2020-05-10 11:29:27 +0800

    第三次更改

--- dome/src/com/zfc/TestMin.java
+++ dome/src/com/zfc/TestMin.java
@@ -2,7 +2,6 @@ package com.zfc;
 
 public class TestMin {
     public static void main(String[] args) {
-        System.out.println("哈哈哈");
-        System.out.println();
+        System.out.println("李大炮");
     }
 }
