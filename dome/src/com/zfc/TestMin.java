65707977e9235c1668d1f561fedd97bcabd5d48f
StarryPlenilune <1205832940@qq.com>
2020-05-10 21:16:14 +0800

    撤回测试1

--- dome/src/com/zfc/TestMin.java
+++ dome/src/com/zfc/TestMin.java
@@ -6,6 +6,6 @@ public class TestMin {
         System.out.println("李大炮");
         System.out.println("李大炮");
         System.out.println("李大炮");
-        System.out.println("周凡程");
+        System.out.println("渣渣辉");
     }
 }
